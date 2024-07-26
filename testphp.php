8d927f92c07d570f7d70a3b067deaa21e5db928b
Usman Musa <usmanmusa1920@gmail.com>
2024-07-26 10:40:04 +0100

    added `in-active-window.html` and `testphp.php` files

---
+++ testphp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My php</title>
+</head>
+<body>
+  <h1>Here is php</h1>
+  <?php phpinfo(); ?>
+  <?php
+    echo "Hello, World!\n";
+  ?>
+</body>
+</html>
